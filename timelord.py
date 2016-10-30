0c533a11fde92b583f11221af593fca988e9b8d9
cloudier <cldtu@yahoo.com.au>
2016-10-30 18:21:09 +1100

    pep8

--- timelord.py
+++ timelord.py
@@ -10,11 +10,11 @@ from pytz import timezone
 from slackclient import SlackClient
 
 
-BOT_ID=""
-SLACK_BOT_TOKEN=""
-with open ("/root/projects/timelord/.bot_tokens", "r") as f:
-	BOT_ID = f.readline().rstrip()
-	SLACK_BOT_TOKEN = f.readline().rstrip()
+BOT_ID = ""
+SLACK_BOT_TOKEN = ""
+with open("/root/projects/timelord/.bot_tokens", "r") as f:
+    BOT_ID = f.readline().rstrip()
+    SLACK_BOT_TOKEN = f.readline().rstrip()
 
 # Timelord's details.
 BOT_NAME = "timelord"
@@ -22,15 +22,13 @@ BOT_NAME = "timelord"
 # Constants.
 AT_BOT = "<@" + BOT_ID + ">"
 DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",
-    "Sunday"]
+        "Sunday"]
 MONTHS = ["January", "February", "March", "April", "May", "June", "July",
-    "August", "September", "October", "November", "December"]
-#MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
-    "Oct", "Nov", "Dec"]
-ORDINAL_NUM = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th",
-    "10th", "11th", "12th", "13th", "14th", "15th", "16th", "17th",
-    "18th", "19th", "20th", "21st", "22nd", "23rd", "24th", "25th",
-    "26th", "27th", "28th", "29th", "30th", "31st"]
+          "August", "September", "October", "November", "December"]
+ORDINAL_NUM = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th",
+               "9th", "10th", "11th", "12th", "13th", "14th", "15th", "16th",
+               "17th", "18th", "19th", "20th", "21st", "22nd", "23rd", "24th",
+               "25th", "26th", "27th", "28th", "29th", "30th", "31st"]
 
 
 # Instantiate Slack & Twilio clients.
@@ -39,10 +37,10 @@ slack_client = SlackClient(SLACK_BOT_TOKEN)
 # Load regions=>timezones map.
 regionsToTimezonesMap = {}
 with open("regionsToTimezones.txt", "r") as f:
-  for line in f:
-      region, zone = line.rstrip().split(",")
-      region = region.lower()
-      regionsToTimezonesMap[region] = zone 
+    for line in f:
+        region, zone = line.rstrip().split(",")
+        region = region.lower()
+        regionsToTimezonesMap[region] = zone
 
 
 def capitaliseRegionNames(region):
@@ -51,15 +49,15 @@ def capitaliseRegionNames(region):
     """
     regionName = region.split()
     doNotCapitalise = set(["of", "es"])
-    for i in xrange(len(regionName)):		# for word in name
+    for i in xrange(len(regionName)):        # for word in name
         if regionName[i] in doNotCapitalise:
-             continue
-        
-        start = 0				# for letter in word
+            continue
+
+        start = 0                # for letter in word
         element = regionName[i]
         if element[start] == "(":
-             start = 1
-        regionName[i] = element[:start] + element[start].upper() + element[start+1:]
+            start = 1
+        regionName[i] = element[:start] + element[start].upper() + element[start + 1:]
     return " ".join(regionName)
 
 
@@ -78,30 +76,30 @@ def getDateTime(command):
         region = capitaliseRegionNames(region)
 
     response = ""
-    if dt == None:
+    if dt is None:
         response = "I'm sorry. I'm so sorry. Region is undefined."
     else:
         # Date and time variables.
         hour, minute = dt.hour, dt.minute
         day, month, year = dt.day, dt.month, dt.year
-        calendarMonth, weekday, ordinalDay = MONTHS[month-1], DAYS[dt.weekday()], ORDINAL_NUM[day-1]
+        calendarMonth, weekday, ordinalDay = MONTHS[month - 1], DAYS[dt.weekday()], ORDINAL_NUM[day - 1]
 
         # Process time.
         AMPM = "am"
         if hour >= 12:
-            hour = hour%12
+            hour = hour % 12
             AMPM = "pm"
         if hour == 0:
             hour = 12
-        
+
         hour = str(hour)
         minute = str(minute)
         if len(minute) == 1:
             minute = "0" + minute
-        
-        #Time response: It is Monday, 2:12am in Sydney!
-        #Date response: It is Monday, 29th October in Sydney!
-        #DT response: It is Monday, 29th October (2:12am) in Sydney!
+
+        # Time response: It is Monday, 2:12am in Sydney!
+        # Date response: It is Monday, 29th October in Sydney!
+        # DT response: It is Monday, 29th October (2:12am) in Sydney!
         time = str(hour) + ":" + str(minute) + AMPM
         date = weekday + ", " + ordinalDay + " " + calendarMonth
         if mode == "time":
@@ -127,7 +125,7 @@ def handle_command(command, channel):
 
     # Colloquial terms for Sydney and SF.
     sf = set(["sf", "sanfran", "san fran", "san francisco", "sanfrancisco", "america", "us"])
-    syd = set(["syd", "sydney", "australia", "aus"]) 
+    syd = set(["syd", "sydney", "australia", "aus"])
 
     if splitCommand[0] == "help":
         response = "Current commands include:\n   @timelord {region}\n   @timelord time {region}\n   @timelord date {region}\n   @timelord dt {region}\n"
@@ -149,13 +147,13 @@ def handle_command(command, channel):
             command = "time san francisco"
             response = getDateTime(command)
         else:
-            catchphrases = ["Nonsense", "When I say run, run.", 
-                            "Reverse the polarity of the neutron flow..", 
+            catchphrases = ["Nonsense", "When I say run, run.",
+                            "Reverse the polarity of the neutron flow..",
                             "Would you like a jelly baby?", "Sorry, I must dash!",
-                            "I wonder...", "Fine.", 
-                            "Probably not the one you expected.", "No more!", 
+                            "I wonder...", "Fine.",
+                            "Probably not the one you expected.", "No more!",
                             "Fantastic!", "Allons-y!", "Geronimo!"]
-            randomNumber = random.randint(0, len(catchphrases)-1)
+            randomNumber = random.randint(0, len(catchphrases) - 1)
             response = catchphrases[randomNumber]
     slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
 
@@ -171,13 +169,12 @@ def parse_slack_output(slack_rtm_output):
         for output in output_list:
             if output and 'text' in output and AT_BOT in output['text']:
                 # return text after the @ mention, whitespace removed
-                return output['text'].split(AT_BOT)[1].strip().lower(), \
-                       output['channel']
+                return output['text'].split(AT_BOT)[1].strip().lower(), output['channel']
     return None, None
 
 
 if __name__ == "__main__":
-    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+    READ_WEBSOCKET_DELAY = 1  # 1 second delay between reading from firehose
     if slack_client.rtm_connect():
         print("timelord connected and running!")
         while True:
